9ffeab77b165dacd3fe838754a43ac91871a12ac
Apprentice <apprentice@dbc-chicago-009.local>
2013-08-12 11:20:39 -0500

    mvp finished

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,6 +1,3 @@
 get '/' do
-  # let user create new short URL, display a list of shortened URLs
-  @urls = Url.all
-  @display = "_front_page"
   erb :index
 end
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,47 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  console.log("Starting...");
+  $("iframe").hide();
+
+  $(document).on('keyup', function(e){
+     if (e.keyCode == 87){
+       movePlayer('#player1_strip')
+       console.log(" W WORKED");
+     }
+
+     if (e.keyCode == 38){
+      movePlayer('#player2_strip')
+      console.log("UP WORKED");
+     }
+  });
 });
+
+var movePlayer = function(player){
+
+ //  if not then do this
+  var active = (player + ' .active')
+  var next = $(active).next();
+  $(active).removeClass('active');
+  $(next).addClass('active');
+
+  gameFinished(player);
+ // check to see if last cell of table is active
+
+ // if yes then say player won
+};
+
+var playerWon = function(player){
+  $('.racer_table').hide();
+  $('iframe').show();
+};
+
+var gameFinished = function(player){
+  var last_cell = (player + ' td:last-child')
+  
+  if ($(last_cell).hasClass('active')){
+    playerWon(player);
+    console.log(player + ' won!');
+    $(document).unbind('keyup');
+  }
+};
